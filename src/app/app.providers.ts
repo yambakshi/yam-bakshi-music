0dab0fb6c6c7781eedb973f8330b537fd22dd91a
yambakshi <yambakshi@gmail.com>
2020-05-18 14:20:16 +0300

    Created Meta Tags Service

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -1,7 +1,9 @@
 import { AppService } from '@services/app.service';
 import { PlatformsService } from '@services/platforms.service';
+import { MetaTagsService } from '@services/meta-tags.service';
 
 export const APP_PROVIDERS = [
     AppService,
-    PlatformsService
+    PlatformsService,
+    MetaTagsService
 ];
---
+++ src/app/services/meta-tags.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+@Injectable()
+export class MetaTagsService {
+    siteName: string = "Yam Bakshi - Official Website";
+    description: string = "Yam Bakshi - Official Website";
+
+    constructor(private meta: Meta ) { }
+
+    setMetaTags({ title, artwork }): void {
+        // Twitter OG
+        this.meta.updateTag({ name: 'twitter:card', content: 'summary' });
+        this.meta.updateTag({ name: 'twitter:site', content: this.siteName });
+        this.meta.updateTag({ name: 'twitter:title', content: title });
+        this.meta.updateTag({ name: 'twitter:description', content: this.description });
+        this.meta.updateTag({ name: 'twitter:image', content: artwork });
+
+        // Facebook OG
+        this.meta.updateTag({ property: 'og:type', content: 'website' });
+        this.meta.updateTag({ property: 'og:site_name', content: this.siteName });
+        this.meta.updateTag({ property: 'og:title', content: title });
+        this.meta.updateTag({ property: 'og:description', content: this.description });
+        this.meta.updateTag({ property: 'og:image', content: artwork });
+        this.meta.updateTag({ property: 'og:url', content: "https://www.yambakshimusic.com" });
+    }
+}
