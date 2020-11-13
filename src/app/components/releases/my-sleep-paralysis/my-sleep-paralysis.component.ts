4f833eddd29e33670cd341272befea83ac157b93
Yam Bakshi <yambakshi@gmail.com>
2020-11-13 15:26:14 +0200

    Released 'My Sleep Paralysis'

--- src/app/components/releases/my-sleep-paralysis/my-sleep-paralysis.component.ts
+++ src/app/components/releases/my-sleep-paralysis/my-sleep-paralysis.component.ts
@@ -9,7 +9,7 @@ export class MySleepParalysisComponent implements OnInit {
   releaseData = {
     meta: {
       name: "My Sleep Paralysis",
-      released: false,
+      released: true,
     },
     graphics: {
       favicon: "v1603385851/releases/my-sleep-paralysis/favicon_cktpgx.ico",
@@ -18,14 +18,14 @@ export class MySleepParalysisComponent implements OnInit {
       og: "v1604051923/releases/my-sleep-paralysis/og_k04ckg.jpg"
     },
     links: {
-      "Spotify": "",
-      "AppleMusic": "",
-      "iTunesStore": "",
-      "Deezer": "",
-      "YouTubeMusic": "",
-      "AmazonMusic": "",
-      "Soundcloud": "https://soundcloud.com/yambakshi/my-sleep-paralysis/s-BQADo",
-      "Bandcamp": ""
+      "Spotify": "https://open.spotify.com/track/3NCrzhT8ma585iIDvCILPV?si=xRRjtABISpm_TmCfXR28Yg",
+      "AppleMusic": "https://music.apple.com/il/album/my-sleep-paralysis/1535847255?i=1535847256",
+      "iTunesStore": "https://music.apple.com/il/album/my-sleep-paralysis/1535847255?i=1535847256",
+      "Deezer": "https://deezer.page.link/Nidb9tx46hGKzMxUA",
+      "YouTubeMusic": "https://www.youtube.com/watch?v=4zGZQvbxhwU&list=OLAK5uy_nbNk4dBto4gNv-bLWpn610p1t3SqSQoQw",
+      "AmazonMusic": "https://www.amazon.com/My-Sleep-Paralysis-Yam-Bakshi/dp/B08L7244RM/ref=sr_1_11?dchild=1&keywords=yam+bakshi&qid=1605260732&sr=8-11",
+      "Soundcloud": "https://soundcloud.com/yambakshi/my-sleep-paralysis",
+      "Bandcamp": "https://yambakshi.bandcamp.com/track/my-sleep-paralysis-2"
     },
     lyrics: [{
       name: 'My Sleep Paralysis',
