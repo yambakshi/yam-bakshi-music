956ff71fdb4886fbb8bab4a3ccceaef589e145ee
Yam Bakshi <yambakshi@gmail.com>
2020-11-18 21:02:29 +0200

    Finished Gallery

--- src/app/components/templates/gallery-template/gallery-template.component.ts
+++ src/app/components/templates/gallery-template/gallery-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Renderer2 } from '@angular/core';
+import { Component, OnInit, Input, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { CloudinaryService } from '@services/cloudinary.service';
 
 @Component({
@@ -12,6 +12,8 @@ import { CloudinaryService } from '@services/cloudinary.service';
 })
 export class GalleryTemplateComponent implements OnInit {
   @Input() images;
+  @ViewChild('galleryImagesContainer') galleryImagesContainer: ElementRef;
+  selectedImage: number = 0;
 
   constructor(
     private renderer: Renderer2,
@@ -21,5 +23,42 @@ export class GalleryTemplateComponent implements OnInit {
   ngOnInit(): void {
       const { prefix } = this.cloudinaryService;
       this.images = this.images.map(imageUrl => `${prefix}${imageUrl}`);
+  }
+
+  ngAfterViewInit(): void {
+    this.galleryImagesContainer.nativeElement.children[this.selectedImage].addEventListener('onLoad', this.onImageLoad.bind(this));
+  }
+
+  nextImage(): void {
+    if (this.selectedImage == this.galleryImagesContainer.nativeElement.children.length - 1) return;
+    this.selectedImage++;
+    this.centerImage();
+  }
+
+  prevImage(): void {
+    if (this.selectedImage == 0) return;
+    this.selectedImage--;
+    this.centerImage();
+  }
+
+  centerImage(): void {
+    let imagesContainerElement = this.galleryImagesContainer.nativeElement;
+    let selectedImage = imagesContainerElement.children[this.selectedImage];
+    let imageWidth = selectedImage.offsetWidth;
+
+    let imageStart = 0;
+    let margin = 100;
+    for (let j = 0; j < this.selectedImage; j++) {
+      imageStart += (imagesContainerElement.children[j].offsetWidth + margin);
+    }
+
+    let imagePos = imageStart + (imageWidth / 2);
+
+    this.renderer.setStyle(this.galleryImagesContainer.nativeElement, 'transform', `translate(${-imagePos}px, 0%)`);
+
+  }
+
+  onImageLoad(): void {
+    this.centerImage();
   }
 }
